a9b94cd402dcd5d9978691aacfb602738d41ba5f
elianiva <dicha.arkana03@gmail.com>
2021-02-11 11:57:52 +0700

    chore: init

---
+++ app/src/main/java/guitest/App.java
@@ -0,0 +1,12 @@
+package guitest;
+
+import main.java.guitest.TFrame;
+
+public class App {
+    public static void main(String[] args) {
+        TFrame window = new TFrame();
+        window.setTitle("Hitung luas / keliling dengan java.awt");
+        window.setSize(400, 300);
+        window.setVisible(true);
+    }
+}
