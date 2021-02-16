371dfdb380d2db2e8b10050c87ccb515bb9bfb09
elianiva <dicha.arkana03@gmail.com>
2021-02-16 13:15:24 +0700

    öooooö

--- app/src/main/java/hitung_gaji/App.java
+++ app/src/main/java/hitung_gaji/App.java
@@ -1,7 +1,16 @@
 package hitung_gaji;
 
+import java.awt.Color;
+
+import main.java.hitung_gaji.MainWindow;
+
 public class App {
+    // This is *kinda* working, idk, couldn't be bothered to fix it
     public static void main(String[] args) {
-        System.out.println("Hello");
+        MainWindow appWindow = new MainWindow();
+        appWindow.setVisible(true);
+        appWindow.setTitle("Perhitungan Gaji");
+        appWindow.setSize(400, 600);
+        appWindow.setBackground(Color.WHITE);
     }
 }
